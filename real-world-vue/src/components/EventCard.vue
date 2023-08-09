d85da11cd91d3d0d18779e9e8081bed8708cbbee
thanap151255 <thanap151255@gmail.com>
2023-08-09 21:42:20 +0700

    4.3

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <RouterLink class="event-link" :to="{name: 'event-detail', params:{id: event?.id}}">
-    <div class="event-class">
-      <div class="event-card">
+      <RouterLink class="text-gray-700 no-underline" :to="{ name: 'event-detail', params: { id: event?.id } }">
+        <div class="mb-4">
+            <div class="p-5 w-64 cursor-pointer border border-gray-800 transform transition-transform duration-200 hover:scale-101 hover:shadow-md">
         <span>@{{ event?.time }} on {{ event?.date }}</span>
         <h4>{{ event?.title }}</h4>
       </div>
