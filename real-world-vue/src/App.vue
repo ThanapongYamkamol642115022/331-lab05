16a4f4f912ef7b83af127753a86d1d1fd8d98d61
thanap151255 <thanap151255@gmail.com>
2023-08-09 21:16:14 +0700

    3.3

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -9,7 +9,7 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-  <header>
+  <header class="max-h-screen leading-normal">
     <div id="flashMessage" v-if="message">
       <h4>{{message}}</h4>
     </div>
@@ -46,10 +46,10 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
-}
+} */
 
 .logo {
   display: block;
